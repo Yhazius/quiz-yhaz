14c007b7d95be55a67e078982bbe55cebae4070a
Jonathan Lozano <j.lozanoredondo@gmail.com>
2015-07-02 19:54:29 +0200

    Desarrollo buscador

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -18,16 +18,31 @@ exports.load = function(req, res, next, quizId) {
 };
 
 // GET /quizes
-
 exports.index = function(req, res) {
-    models.Quiz.findAll().then(function(quizes) {
-        res.render('quizes/index.ejs', {
-            quizes: quizes
-        });
-    }).catch(function(error) {
-        next(error);
-    })
+    if (req.query.busqueda) {
+        var busqueda = (req.query.busqueda).replace(" ", "%");
+        models.Quiz.findAll({
+            where: ["pregunta like ?", '%' + busqueda + '%'],
+            order: 'pregunta ASC'
+        }).then(function(quizes) {
+            res.render('quizes/index.ejs', {
+                quizes: quizes
+            });
+        }).catch(function(error) {
+            next(error);
+        })
+    } else {
+        models.Quiz.findAll().then(function(quizes) {
+            res.render('quizes/index.ejs', {
+                quizes: quizes
+            });
+        }).catch(function(error) {
+            next(error);
+        })
+    }
 };
+
+
 
 // GET /quizes/:id
 
